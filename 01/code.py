e17509c651d606488b4c72972f5bb95591bf819f
Nick Sawe <codekip@outlook.com>
2018-12-03 19:38:13 +0000

    Day 1 part one

--- 01/code.py
+++ 01/code.py
@@ -2,17 +2,41 @@ import operator
 import os
 import sys
 from functools import reduce
+from itertools import accumulate, cycle
+
+'''
+# Part 2
+from itertools import accumulate, cycle
+seen = set()
+print(next(f for f in accumulate(cycle(changes)) if f in seen or seen.add(f)))
+'''
 
 
-def calculate(data):
+def calculate(data, thefunc=''):
     no_newline = [x.split('\n') for x in data]
     flat = list(filter(lambda x: x != '', [
                 item for sublist in no_newline for item in sublist]))
-    return add(flat)
+    if thefunc != '':
+        return thefunc(flat)
+    else:
+        return flat
 
 
 def add(lst):
     return reduce(lambda x, y: int(x) + int(y), lst, 0)
+
+
+def add_acc(lst):
+    return lambda x, y: int(x) + int(y), lst
+
+
+def twice(lst):
+    data = calculate(lst)
+    return 0
+
+
+def doit(data):
+    pass
 
 
 def openfile():
--- 01/test.py
+++ 01/test.py
@@ -5,15 +5,15 @@ from code import *
 class TestCase(unittest.TestCase):
 
     def test_a(self):
-        self.assertEqual(calculate(['+1']), 1)
-        self.assertEqual(calculate(['+15']), 15)
-        self.assertEqual(calculate(['+15\n', '-14\n']), 1)
+        self.assertEqual(calculate(['+1'], add), 1)
+        self.assertEqual(calculate(['+15'], add), 15)
+        self.assertEqual(calculate(['+15\n', '-14\n'], add), 1)
 
     def test_add(self):
         self.assertEqual(add([1, 3, 3]), 7)
 
     def test_input(self):
-        self.assertEqual(calculate(openfile()), 439)
+        self.assertEqual(calculate(openfile(), add), 439)
 
 
 if __name__ == "__main__":
