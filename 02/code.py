dc47f9f7baf7f1e046a78d89227aaa55e43ac635
Nick Sawe <skelmdev@gmail.com>
2018-12-05 15:39:44 +0000

    Day 2 part 1

--- 02/code.py
+++ 02/code.py
@@ -1,16 +1,39 @@
 import os
-from collections import Counter, defaultdict
+from collections import defaultdict, OrderedDict
 
 
 def count_repetitions(str):
-    cnt = Counter(str)
-    filtered = dict(filter(lambda x: x[1] in [2, 3], cnt.items()))
-    print(filtered)
+    ans = OrderedDict()
+    count = defaultdict(int)
+    for letter in str:
+        count[letter] += 1
+
+    twice = list(filter(lambda x: x == 2, count.values()))
+    three = list(filter(lambda x: x == 3, count.values()))
+
+    ans["twice"] = 1 if len(twice) > 0 else 0
+    ans["three"] = 1 if len(three) > 0 else 0
+
+    return dict(ans)
+
+
+def result(lst):
+    twos = 0
+    threes = 0
+
+    all = lst.split("\n")
+    for item in all:
+        ans = count_repetitions(item)
+        twos += ans["twice"]
+        threes += ans["three"]
+
+    return twos * threes
 
 
 def main():
     with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r") as f:
         input = f.read()
+        print(result(input))
 
 
 if __name__ == "__main__":
--- 02/tests.py
+++ 02/tests.py
@@ -1,15 +1,20 @@
 import unittest
 from code import *
 
-
 class TestPartOne(unittest.TestCase):
+    # @unittest.skip("temp skip")
     def test_count_repetitions(self):
-        # self.assertEqual(count_repetitions("abcdef"), {"abcdef": {"2": 0, "3": 0}})
-        # self.assertEqual(count_repetitions("bababc"), {"bababc": {"2": 1, "3": 1}})
-        # self.assertEqual(count_repetitions("abbcde"), "b")
-        # self.assertEqual(count_repetitions("abcdef"), None)
-        self.assertEqual(count_repetitions("abbbccde"), "b")
-        # self.assertEqual(count_repetitions("abcccd"), None)
+        self.assertEqual(count_repetitions("abcdef"), {"twice": 0, "three": 0})
+        self.assertEqual(count_repetitions("bababc"), {"twice": 1, "three": 1})
+        self.assertEqual(count_repetitions("abbcde"), {"twice": 1, "three": 0})
+        self.assertEqual(count_repetitions("abcccd"), {"twice": 0, "three": 1})
+        self.assertEqual(count_repetitions("aabcdd"), {"twice": 1, "three": 0})
+        self.assertEqual(count_repetitions("abcdee"), {"twice": 1, "three": 0})
+        self.assertEqual(count_repetitions("ababab"), {"twice": 0, "three": 1})
+
+    def test_list(self):
+        lss = "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab"
+        self.assertEqual(result(lss), 12)
 
 
 if __name__ == "__main__":
